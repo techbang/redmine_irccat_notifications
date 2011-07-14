24439ff972c3297816dab633000b2e07f47c3743
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2011-07-14 15:23:53 +0800

    修改訊息身分變數

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -4,8 +4,8 @@ class NotifierHook < Redmine::Hook::Listener
   def controller_issues_new_after_save(context = { })
     @project = context[:project]
     @issue = context[:issue]
-    @user = @issue.author
-    speak "#{@user.lastname} #{@user.firstname} 建立主旨:「#{@issue.subject}」. 狀態:「#{@issue.status.name}」. 意見:「#{truncate_words(@issue.description)}」"
+    @author = @issue.author
+    speak "#{@author.lastname} #{@author.firstname} 建立主旨:「#{@issue.subject}」. 狀態:「#{@issue.status.name}」. 意見:「#{truncate_words(@issue.description)}」"
     speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
   end
   
@@ -13,32 +13,32 @@ class NotifierHook < Redmine::Hook::Listener
     @project = context[:project]
     @issue = context[:issue]
     @journal = context[:journal]
-    @user = @journal.user
-    speak "#{@user.lastname} #{@user.firstname} 編輯主旨:「#{@issue.subject}」. 狀態:「#{@issue.status.name}」. 意見:「#{truncate_words(@journal.notes)}」"
+    @editor = @journal.editor
+    speak "#{@editor.lastname} #{@editor.firstname} 編輯主旨:「#{@issue.subject}」. 狀態:「#{@issue.status.name}」. 意見:「#{truncate_words(@journal.notes)}」"
     speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
   def controller_messages_new_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
-    @user = @message.author
-    speak "#{@user.lastname} #{@user.firstname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
+    @author = @message.author
+    speak "#{@author.lastname} #{@author.firstname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
     speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
   
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
-    @user = @message.author
-    speak "#{@user.lastname} #{@user.firstname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
+    @author = @message.author
+    speak "#{@author.lastname} #{@author.firstname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
     speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
   
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
-    @user = @page.content.author
-    speak "#{@user.lastname} #{@user.firstname} edited the wiki「#{@page.pretty_title}」on #{@project.name}."
+    @author = @page.content.author
+    speak "#{@author.lastname} #{@author.firstname} edited the wiki「#{@page.pretty_title}」on #{@project.name}."
     speak "網址: http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
   end
 
