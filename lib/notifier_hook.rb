6dea5dcea5bb20b9cd9f6010ce9bd4f173c84726
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2011-07-14 16:41:41 +0800

    加入 編輯後狀態的變化

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -9,14 +9,15 @@ class NotifierHook < Redmine::Hook::Listener
     speak "#{@author.lastname} #{@author.firstname} 建立主旨:「#{@issue.subject}」. 狀態:「#{@issue.status.name}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@issue.description)}」"
     speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
   end
-  
+
   def controller_issues_edit_before_save(context = { })
     @project = context[:project]
     @issue = context[:issue]
     @journal = context[:journal]
     @editor = @journal.user
     @assigned_message = @issue.assigned_to.nil? ? "無" : "#{@issue.assigned_to.lastname} #{@issue.assigned_to.firstname}"
-    speak "#{@editor.lastname} #{@editor.firstname} 編輯主旨:「#{@issue.subject}」. 狀態:「#{@issue.status.name}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@journal.notes)}」"
+    @status_message = issue_status_changed?(@issue)
+    speak "#{@editor.lastname} #{@editor.firstname} 編輯主旨:「#{@issue.subject}」. 狀態:「#{@status_message}」. 分派給:「#{@assigned_message}」. 意見:「#{truncate_words(@journal.notes)}」"
     speak "網址: http://#{Setting.host_name}/issues/#{@issue.id}"
   end
 
@@ -27,7 +28,7 @@ class NotifierHook < Redmine::Hook::Listener
     speak "#{@author.lastname} #{@author.firstname} wrote a new message「#{@message.subject}」on #{@project.name}:「#{truncate_words(@message.content)}」"
     speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
-  
+
   def controller_messages_reply_after_save(context = { })
     @project = context[:project]
     @message = context[:message]
@@ -35,7 +36,7 @@ class NotifierHook < Redmine::Hook::Listener
     speak "#{@author.lastname} #{@author.firstname} replied a message「#{@message.subject}」on #{@project.name}: 「#{truncate_words(@message.content)}」"
     speak "網址: http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
   end
-  
+
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
@@ -55,4 +56,14 @@ private
     words = text.split()
     words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
   end
+
+  def issue_status_changed?(issue)
+    if issue.status_id_changed?
+      old_status = IssueStatu.find(issue.status_id_was)
+      "從 #{old_status.name} 變更為 #{issue.status.name}"
+    else
+      "#{issue.status.name}"
+    end
+  end
+
 end
