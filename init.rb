f3229e2528c5b2d0cd0812870559edfb0f85dbdc
v1nc3ntlaw <v1nc3ntlaw@gmail.com>
2012-04-22 22:42:07 +0800

    add utf-8 encoding to *.rb files

--- init.rb
+++ init.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'redmine'
 require_dependency 'notifier_hook'
 
--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -1,3 +1,4 @@
+# -*- encoding : utf-8 -*-
 require 'actionpack'
 class NotifierHook < Redmine::Hook::Listener
 
